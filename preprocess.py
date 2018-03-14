9788367595d0b04e718f0feb0d5e8aac73919367
Derek Kaknes <derek.kaknes@gmail.com>
2018-03-14 12:35:27 -0400

    Updates for feature augmentation

--- preprocess.py
+++ preprocess.py
@@ -1,25 +1,30 @@
 from lib.utils import RamanSample
 import glob
+import sys
 import csv
 import numpy as np
 import warnings
 
 
-raw_files = glob.glob("data/*/*RAW*")
-samples = map(RamanSample.get_spectrum_and_label_from_file, raw_files)
+def run(outfile):
+    raw_files = glob.glob("data/*/*RAW*")
+    samples = map(RamanSample.get_spectrum_and_label_from_file, raw_files)
 
-with open("data/processed/sample.csv", "w") as f:
-    writer = csv.writer(f)
-    for s in samples:
-        spectrum_matrix = s[0]
-        label = s[1]
-        try:
-            new_spec = RamanSample.conform_to_range(
-                    spectrum_matrix, 500, 1500, 0.5
-                    )
-            writer.writerow([label] + list(new_spec))
-        except IndexError:
-            warnings.warn("IndexError")
-            pass
+    with open(outfile, "w") as f:
+        writer = csv.writer(f)
+        for s in samples:
+            spectrum_matrix = s[0]
+            label = s[1]
+            try:
+                new_spec = RamanSample.conform_to_range(
+                        spectrum_matrix, 400, 1800, 2.0
+                        )
+                writer.writerow([label] + list(new_spec))
+            except IndexError:
+                warnings.warn("IndexError")
+                pass
 
+if __name__ == "__main__":
+    outfile = sys.argv[1]
+    run(outfile)
 
